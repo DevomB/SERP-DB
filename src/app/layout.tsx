bff7c8418d3679a23b2ce8cc702a0124ced3901b
Devom Brahmbhatt <Devom.b@yahoo.com>
2024-08-15 23:21:39 -0700

    Revert "Changes in a long time"
    This reverts commit 93e44eeda7a89b2c62ea5e46d48c9eb4e32a85dd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import DevToolDisabler from "../../components/DevToolDisabler/DevToolDisabler";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "SERP DB",
