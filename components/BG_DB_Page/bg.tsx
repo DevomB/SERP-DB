4dc858d1436e2b56329f6996c9eaeaedee3ae845
Devom Brahmbhatt <Devom.b@yahoo.com>
2024-12-18 22:13:43 -0800

    Added Assets and work on new thing

---
+++ components/BG_DB_Page/bg.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+
+const BG_DB_Page = () => {
+
+
+    return (
+        <Image src={aaa}></Image>
+    )
+}
+
+export default BG_DB_Page;
--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,3 +1,8 @@
+import SearchBar from "@/components/SearchBar/SearchBar";
+import ProjectCard from "@/components/ProjCard/ProjCard";
+
+
 export default function DB_Page() {
 	
+    return 0;
 }
